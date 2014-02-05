25f74dcf2cb1e31add0841acb5e61b9703ce685a
Lindsey Sugino <lindseysugino@gmail.com>
2014-02-05 13:39:25 -0800

    updated route to match about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Lindseysugino::Application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/home'
-  get 'static_pages/about'
+  get '/about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
